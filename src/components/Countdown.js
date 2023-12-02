6336f6af645429b8dddfe49f4e37cadf32865531
Tanmay Adithya <marnitanmayadithya@gmail.com>
2023-12-02 22:53:35 +0530

    refactor: Change countdown timer font and remove scroll bar for chromium browsers

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -6,7 +6,7 @@ export default function Countdown() {
     return <section className="countdown-section">
         <div className="countdown">
             <h1>Hackathon<br />begins in</h1>
-            <iframe title="countdown" className="countdown-date" src="https://www.tickcounter.com/widget/countdown/4628913"></iframe>
+            <iframe title="countdown" className="countdown-date" src="https://www.tickcounter.com/widget/countdown/4659104"></iframe>
         </div>
     </section>;
 }
