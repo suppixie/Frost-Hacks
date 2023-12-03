e0c0d516be8e9fca693e316bb2b9b73fe0df33a0
Srikanth Reddy <sriors123@gmail.com>
2023-12-03 23:14:36 +0530

    Update Countdown.js

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -5,7 +5,7 @@ export default function Countdown() {
 
     return <section className="countdown-section">
         <div className="countdown">
-            <h1 style={}>Hackathon<br />begins in</h1>
+            <h1>Hackathon<br />begins in</h1>
             <iframe title="countdown" className="countdown-date" src="https://www.tickcounter.com/widget/countdown/4662031"></iframe>
         </div>
     </section>;
